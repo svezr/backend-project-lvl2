4e0b4848d11de8c63e8802922d1a0e460d8557e1
svezr <svezr@yandex.ru>
2020-03-18 18:43:51 +0300

    add arguments and action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,15 @@ import program from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
   .option('-V, --version', 'output the version number')
   .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .action((firstConfig, secondConfig) => {
+    // console.log(firstConfig, secondConfig)
+  })
   .parse(process.argv);
+
+
 
   // console.log(program.args);
