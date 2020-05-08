04e3e0d617512930ef8b1ef06c3521ba33a0bca3
svezr_wrk <svezzzr@gmail.com>
2020-05-08 17:01:52 +0300

    temp

--- src/index.js
+++ src/index.js
@@ -1,62 +1,163 @@
 import getObjectFromFile from './parsers';
+import _ from 'lodash'
 
-const normalizeObject = (obj) => {
-  const entries = Object.entries(obj);
 
-  const fn = (acc, item) => {
-    const [objectKey, objectValue] = item;
-
-    acc[objectKey] = objectValue;
-    return acc;
-  };
-
-  return entries.reduce(fn, {});
-};
-
-const buildAnswer = (objectBefore, objectAfter, notChangedKeys, changedKeys,
-  deletedKeys, addedKeys) => {
-  const valuesNotChanged = notChangedKeys.slice().sort().reduce((prev, item) => `${prev}\n    ${item}: ${objectBefore[item]}`, '');
-
-  const valuesChanged = changedKeys.slice().sort().reduce((prev, item) => `${prev}\n  - ${item}: ${objectBefore[item]}\n  + ${item}: ${objectAfter[item]}`, '');
-
-  const valuesDeleted = deletedKeys.slice().sort().reduce((prev, item) => `${prev}\n  - ${item}: ${objectBefore[item]}`, '');
-
-  const valuesAdded = addedKeys.slice().sort().reduce((prev, item) => `${prev}\n  + ${item}: ${objectAfter[item]}`, '');
-
-  return `{${valuesNotChanged}${valuesChanged}${valuesDeleted}${valuesAdded}\n}`;
-};
-
-const genDiff = (filename1, filename2) => {
-  const rawObjectBefore = getObjectFromFile(filename1);
-  const rawObjectAfter = getObjectFromFile(filename2);
-
-  if (!rawObjectBefore || !rawObjectAfter) {
-    console.log('Error reading file!');
-    return;
+const getPrintValue = (item, margin = 0) => {
+  if (!_.isPlainObject(item)) {
+    return item;
   }
 
-  const objectBefore = normalizeObject(rawObjectBefore);
-  const objectAfter = normalizeObject(rawObjectAfter);
+  let result = '{';
+  const objectKeys = Object.keys(item);
 
-  const keysBefore = Object.keys(objectBefore);
-  const keysAfter = Object.keys(objectAfter);
+  for (let key of objectKeys) {
+    const keyValue = item[key];
+    const value = _.isPlainObject(keyValue) ? getPrintValue(keyValue, margin) : keyValue;
 
-  const allKeys = [...keysBefore, ...keysAfter].reduce((acc, item) => (
-    acc.includes(item) ? acc : [...acc, item]), []);
+    result += '\n' + ' '.repeat(margin+4) + `${key}: ${value}`;
+  }
 
-  const addedKeys = keysAfter.filter((item) => !keysBefore.includes(item));
-  const deletedKeys = keysBefore.filter((item) => !keysAfter.includes(item));
-  const changedKeys = allKeys.filter((item) => (keysAfter.includes(item)
-          && keysBefore.includes(item) && (objectBefore[item] !== objectAfter[item])));
-
-  const notChangedKeys = keysBefore.filter((item) => (!changedKeys.includes(item)
-          && !deletedKeys.includes(item)));
-
-  return buildAnswer(objectBefore, objectAfter, notChangedKeys, changedKeys,
-    deletedKeys, addedKeys);
+  result += '\n' + ' '.repeat(margin) + '}';
+  return result;
 };
 
+const getOperationType = (beforeObject, afterObject, key) => {
+  let operation = 'none';
+
+  const bothItemsAreObjects = _.isPlainObject(beforeItem) && _.isPlainObject(afterItem);
+
+  if (!(_.has(beforeObject, key)) && _.has(afterObject, key)) {
+    operation = 'add';
+  }
+
+  if (_.has(beforeObject, key) && !(_.has(afterObject, key))) {
+    operation = 'remove';
+  }
+
+  if ((_.has(beforeObject, key) && _.has(afterObject, key)) && !(_.isEqual(beforeObject[key], afterObject[key]))) {
+    operation = 'change';
+  };
+
+  if (operation === 'change') && bothItemsAreObjects {
+    operation = 'changeObjects';
+  }
+
+  return operation;
+};
+
+
+const diff = (beforeObject, afterObject, margin = 0) => {
+  let s =  '{';
+
+  const outputPrefix = '\n' + ' '.repeat(margin);
+
+  const keysBefore = Object.keys(beforeObject);
+  const keysAfter = Object.keys(afterObject);
+  const keys = _.uniq([...keysBefore, ...keysAfter]).sort();
+
+  const operationsPrefix = {
+    none: `${outputPrefix}    `,
+    add: `${outputPrefix}  + `,
+    remove: `${outputPrefix}  - `,
+    change: `${outputPrefix}  - `,
+    changedObjects: `${outputPrefix}    `,
+  };
+
+  
+
+  for (let i = 0; i < keys.length; i += 1) {
+    const beforeItem = beforeObject[key];
+    const afterItem = afterObject[key];
+
+    const key = keys[i];
+    const operation = getOperationType(beforeObject, afterObject, key);
+    const prefix = `${operationsPrefix[operation]}${key}: `;
+
+    switch (operation) {
+      case 'none':
+        s += `${prefix}${getPrintValue(beforeItem, margin + 4)}`;
+        break;
+      case 'add':
+        s += `${prefix}${getPrintValue(afterItem, margin + 4)}`;
+        break;
+      case 'remove':
+        s += `${prefix}${getPrintValue(beforeItem, margin + 4)}`;
+        break
+      case 'change':
+        s += `${prefix}${getPrintValue(beforeItem, margin + 4)}`;
+        s += `${prefix}${getPrintValue(afterItem, margin + 4)}`;
+        break;
+      case 'changeObjects':
+        s += `${prefix}${diff(beforeItem, afterItem, margin + 4)}`;
+        break;
+    };
+  }
+
+  s += `${outputPrefix}}`;
+
+  return s;
+}
+
+
+
+// const normalizeObject = (obj) => {
+//   const entries = Object.entries(obj);
+
+//   const fn = (acc, item) => {
+//     const [objectKey, objectValue] = item;
+
+//     acc[objectKey] = objectValue;
+//     return acc;
+//   };
+
+//   return entries.reduce(fn, {});
+// };
+
+// const buildAnswer = (objectBefore, objectAfter, notChangedKeys, changedKeys,
+//   deletedKeys, addedKeys) => {
+//   const valuesNotChanged = notChangedKeys.slice().sort().reduce((prev, item) => `${prev}\n    ${item}: ${objectBefore[item]}`, '');
+
+//   const valuesChanged = changedKeys.slice().sort().reduce((prev, item) => `${prev}\n  - ${item}: ${objectBefore[item]}\n  + ${item}: ${objectAfter[item]}`, '');
+
+//   const valuesDeleted = deletedKeys.slice().sort().reduce((prev, item) => `${prev}\n  - ${item}: ${objectBefore[item]}`, '');
+
+//   const valuesAdded = addedKeys.slice().sort().reduce((prev, item) => `${prev}\n  + ${item}: ${objectAfter[item]}`, '');
+
+//   return `{${valuesNotChanged}${valuesChanged}${valuesDeleted}${valuesAdded}\n}`;
+// };
+
+// const genDiff = (filename1, filename2) => {
+//   const rawObjectBefore = getObjectFromFile(filename1);
+//   const rawObjectAfter = getObjectFromFile(filename2);
+
+//   if (!rawObjectBefore || !rawObjectAfter) {
+//     console.log('Error reading file!');
+//     return;
+//   }
+
+//   const objectBefore = normalizeObject(rawObjectBefore);
+//   const objectAfter = normalizeObject(rawObjectAfter);
+
+//   const keysBefore = Object.keys(objectBefore);
+//   const keysAfter = Object.keys(objectAfter);
+
+//   const allKeys = [...keysBefore, ...keysAfter].reduce((acc, item) => (
+//     acc.includes(item) ? acc : [...acc, item]), []);
+
+//   const addedKeys = keysAfter.filter((item) => !keysBefore.includes(item));
+//   const deletedKeys = keysBefore.filter((item) => !keysAfter.includes(item));
+//   const changedKeys = allKeys.filter((item) => (keysAfter.includes(item)
+//           && keysBefore.includes(item) && (objectBefore[item] !== objectAfter[item])));
+
+//   const notChangedKeys = keysBefore.filter((item) => (!changedKeys.includes(item)
+//           && !deletedKeys.includes(item)));
+
+//   return buildAnswer(objectBefore, objectAfter, notChangedKeys, changedKeys,
+//     deletedKeys, addedKeys);
+// };
+
 export default genDiff;
+
 
 // TODO: новый алгоритм
 
