de5808cd7502cce65ac1c68af098cd424295ee4f
svezr <svezr@yandex.ru>
2020-05-19 21:14:25 +0300

    added JSON, YAML, INI tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,7 +2,7 @@ import { expect } from '@jest/globals';
 import path from 'path';
 import { readFileSync } from 'fs';
 
-import { genDiff } from '../dist';
+import { genDiff } from '../src';
 
 const getFixturesDirectoryPath = () => {
   const delimiter = path.sep;
@@ -13,21 +13,38 @@ const getFixturesDirectoryPath = () => {
 
 const getFixtureFilePath = (fileName) => path.resolve(getFixturesDirectoryPath(), fileName);
 
-// const testCorrectResults = {
-//   add: '{\n    beforeValue: beforeValue\n    beforeValue1: beforeValue1\n    beforeValue2: beforeValue2\n  + valueNumber: 22\n  + valueString: value1\n}',
-//   remove: '{\n    beforeValue: beforeValue\n    beforeValue1: beforeValue1\n  - beforeValue2: beforeValue2\n}',
-//   change: '{\n  - beforeValue: beforeValue\n  + beforeValue: beforeValueChanged\n  - beforeValue1: beforeValue1\n  + beforeValue1: beforeValue1Changed\n  - beforeValue2: beforeValue2\n  + beforeValue2: beforeValue2Changed\n}',
-//   combo: '{\n    beforeValue: beforeValue\n  - beforeValue2: beforeValue2\n  + beforeValue2: beforeValue2Changed\n  - beforeValue1: beforeValue1\n  + valueNumber: 22\n  + valueString: value1\n}',
-// };
-
-test('test1', () => {
+test('test JSON <-> JSON', () => {
   const pathFileBefore = getFixtureFilePath('structBefore.json');
   const pathFileAddTest = getFixtureFilePath('structAfter1.json');
   const pathFileAddResult = getFixtureFilePath('structTestResults.txt');
 
-  const fileAndResult = readFileSync(pathFileAddResult, 'utf8');
+  const correctData = readFileSync(pathFileAddResult, 'utf8');
 
   const stylishedGenDiff = genDiff(pathFileBefore, pathFileAddTest, 'stylish').trim();
 
-  expect(stylishedGenDiff).toBe(fileAndResult);
+  expect(stylishedGenDiff).toBe(correctData);
+});
+
+test('test JSON <-> YAML', () => {
+  const pathFileBefore = getFixtureFilePath('structBefore.json');
+  const pathFileAddTest = getFixtureFilePath('structAfter2.yml');
+  const pathFileAddResult = getFixtureFilePath('structTestResults.txt');
+
+  const correctData = readFileSync(pathFileAddResult, 'utf8');
+
+  const stylishedGenDiff = genDiff(pathFileBefore, pathFileAddTest, 'stylish').trim();
+
+  expect(stylishedGenDiff).toBe(correctData);
+});
+
+test('test JSON <-> INI', () => {
+  const pathFileBefore = getFixtureFilePath('structBefore.json');
+  const pathFileAddTest = getFixtureFilePath('structAfter3.ini');
+  const pathFileAddResult = getFixtureFilePath('structTestResults.txt');
+
+  const correctData = readFileSync(pathFileAddResult, 'utf8');
+
+  const stylishedGenDiff = genDiff(pathFileBefore, pathFileAddTest, 'stylish').trim();
+
+  expect(stylishedGenDiff).toBe(correctData);
 });
