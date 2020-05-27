37bc47690e20524e19518464f2c7c1b82b045124
svezr_wrk <svezzzr@gmail.com>
2020-05-27 14:40:20 +0300

    Added lodash module

---
+++ src/formatters/json.js
@@ -0,0 +1,50 @@
+// import _ from 'lodash';
+
+const json = (diff) => {
+  const result = '' + diff;
+  return result;
+};
+
+export default json;
+
+
+// [
+//  {
+//    "filePath":"D:\\dev\\projects\\backend-project-lvl2\\babel.config.js",
+//    "messages":[],
+//    "fixableWarningCount":0
+//  },
+//  {
+//    "filePath":"\\index.js",
+//    "messages":
+//      [
+//        {
+//         "ruleId":"no-trailing-spaces",
+//         "fix":{
+//           "range":[137,140],
+//           "text":""
+//          }
+//         }
+//      ],
+//  },
+//  {
+//    "fixableWarningCount":0
+//  },
+//  {
+//    "fixableWarningCount":0
+//  },
+//  {
+//    "fixableWarningCount":0
+//  },
+//  {
+//    "filePath":"D:\\dev\\projects\\backend-project-lvl2\\src\\parsers.js",
+//    "messages":[],
+//    "fixableWarningCount":0
+//  },
+//  {
+//    "filePath":"D:\\dev\\projects\\backend-project-lvl2\\__tests__\\gendiff.test.js",
+//    "messages":[],
+//    "errorCount":0,
+//    "fixableWarningCount":0
+//  }
+// ]
