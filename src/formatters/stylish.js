94d2d397ab3d680027e25685c657e68be50b4dec
svezr <svezr@yandex.ru>
2020-06-02 23:05:13 +0300

    changed linter settings

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -16,7 +16,8 @@ const stringifyPlainObject = (object, margin = 0) => {
 
     if (isObject) {
       s += prefix(margin, ' ', key);
-      s += valueIsObject ? '{' + stringifyPlainObject(object[key], margin + 4) + suffix(margin + 2) + '}' : object[key];
+      // s += valueIsObject ? '{' + stringifyPlainObject(object[key], margin + 4) + suffix(margin + 2) + '}' : object[key];
+      s += valueIsObject ? `{${stringifyPlainObject(object[key], margin + 4)}${suffix(margin + 2)}}` : object[key];
     } else {
       s += key;
     }
@@ -38,31 +39,31 @@ const createLine = (item, margin) => {
   switch (operation) {
     case 'add':
       if (_.isPlainObject(valueAfter)) {
-        line = prefix(margin, '+', key) + '{' + stringifyPlainObject(valueAfter, margin + 4) + suffix(margin + 2) + '}';
+        line = `${prefix(margin, '+', key)}{${stringifyPlainObject(valueAfter, margin + 4)}${suffix(margin + 2)}}`;
       } else {
         line = prefix(margin, '+', key) + valueAfter;
       }
       break;
     case 'remove':
       if (_.isPlainObject(valueBefore)) {
-        line = prefix(margin, '-', key) + '{' + stringifyPlainObject(valueBefore, margin + 4) + suffix(margin + 2) + '}';
+        line = `${prefix(margin, '-', key)}{${stringifyPlainObject(valueBefore, margin + 4)}${suffix(margin + 2)}}`;
       } else {
         line = prefix(margin, '-', key) + valueBefore;
       }
       break;
     case 'modify':
       if (_.has(item, 'children')) {
-        line = prefix(margin, ' ', key) + '{';
+        line = `${prefix(margin, ' ', key)}{`;
 
         for (let i = 0; i < item.children.length; i += 1) {
           const child = item.children[i];
           line += createLine(child, margin + 4);
         }
 
-        line += suffix(margin + 2) + '}';
+        line += `${suffix(margin + 2)}}`;
       } else {
-        const valueBeforeModify = (_.isPlainObject(valueBefore)) ? '{' + stringifyPlainObject(valueBefore, margin + 4) + suffix(margin + 2) + '}' : valueBefore;
-        const valueAfterModify = (_.isPlainObject(valueAfter)) ? '{' + stringifyPlainObject(valueAfter, margin + 4) + suffix(margin + 2) + '}' : valueAfter;
+        const valueBeforeModify = (_.isPlainObject(valueBefore)) ? `{${stringifyPlainObject(valueBefore, margin + 4)}${suffix(margin + 2)}}` : valueBefore;
+        const valueAfterModify = (_.isPlainObject(valueAfter)) ? `{${stringifyPlainObject(valueAfter, margin + 4)}${suffix(margin + 2)}}` : valueAfter;
 
         line = prefix(margin, '-', key) + valueBeforeModify;
         line += prefix(margin, '+', key) + valueAfterModify;
@@ -71,7 +72,7 @@ const createLine = (item, margin) => {
       break;
     default:
       if (_.isPlainObject(valueBefore)) {
-        line = prefix(margin, ' ', key) + '{' + stringifyPlainObject(valueBefore, margin + 4) + suffix(margin + 2) + '}';
+        line = `${prefix(margin, ' ', key)}{${stringifyPlainObject(valueBefore, margin + 4)}${suffix(margin + 2)}}`;
       } else {
         line = prefix(margin, ' ', key) + valueBefore;
       }
@@ -90,7 +91,7 @@ const stylish = (diff, margin = 0) => {
     stylishedDiff += createLine(item, margin + 2);
   }
 
-  return stylishedDiff + suffix(margin) + '}';
+  return `${stylishedDiff}${suffix(margin)}}`;
 };
 
 export default stylish;
